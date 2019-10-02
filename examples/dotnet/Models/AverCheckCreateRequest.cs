0b54413eab78f7fc2b218819e71b3a543e624eb2
Gubanotorious <6355333+gubanotorious@users.noreply.github.com>
2019-10-02 11:37:25 -0400

    Updated examples for v1.1 API changes

--- examples/dotnet/Models/AverCheckCreateRequest.cs
+++ examples/dotnet/Models/AverCheckCreateRequest.cs
@@ -8,16 +8,19 @@ namespace Aver.ApiIntegration.Models
 {
     public class AverCheckCreateRequest
     {
-        [JsonProperty(PropertyName = "uniqueId")]
-        public string UniqueId { get; set; }
-
         [JsonProperty(PropertyName = "groupId")]
         public string GroupId { get; set; }
+
+        [JsonProperty(PropertyName = "thirdPartyIdentifier")]
+        public string ThirdPartyIdentifier { get; set; }
+
+        [JsonProperty(PropertyName = "email")]
+        public string EmailAddress { get; set; }
 
         [JsonProperty(PropertyName = "returnUrl")]
         public string ReturnUrl { get; set; }
 
-        [JsonProperty(PropertyName = "email")]
-        public string Email { get; set; }
+        [JsonProperty(PropertyName = "language")]
+        public string Language { get; set; }
     }
 }
