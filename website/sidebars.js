7cab1d54612e9a0feed996216ddcbad05bc64dcc
Gubanotorious <gubanotorious@gmail.com>
2022-01-19 13:11:42 -0500

    Remove sidebars for now until ready to publish...

--- website/sidebars.js
+++ website/sidebars.js
@@ -1,7 +1,6 @@
 module.exports = {
   someSidebar: {
     'Getting Started': ['overview','quickstart','accessing','webhook'],
-    'Endpoints':['auth','check','check-advanced','watchlist'],
-    'Reference':['checkgroups', 'readingcheckresults', 'readingwatchlistresults']
+    'Endpoints':['auth','check','check-advanced','watchlist']
   },
 };
